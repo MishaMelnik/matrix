e4c936207bb87e3c5cf4ae4b27adde34456d649c
misha <melnik_mo@groupbwt.com>
2022-05-23 09:40:00 +0300

    The project is prepared for work

---
+++ src/components/Platform/Platform.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Platform.scss';
+
+const Platform = () => {
+  return (
+    <div className="container">
+      <div></div>
+    </div>
+  );
+};
+
+export default Platform;
